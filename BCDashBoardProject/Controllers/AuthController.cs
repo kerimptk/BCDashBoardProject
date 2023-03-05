b8ce48308e31ef63b9597b08702a007a7d8e1ce5
kerim <kerim@MERVIKERIM>
2023-03-05 19:52:58 +0300

    Add project files.

---
+++ BCDashBoardProject/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using BCDashBoardProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BCDashBoardProject.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public AuthController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
