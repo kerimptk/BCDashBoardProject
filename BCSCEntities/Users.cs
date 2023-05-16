303dad587a10f5a164c2aa0e801d8f6e06d5b01a
kerimpotuk@gmail.com <kerimpotuk@gmail.com>
2023-05-16 13:35:53 +0300

    Class'ların eklenmesi ve User Entity

---
+++ BCSCEntities/Users.cs
@@ -0,0 +1,10 @@
+﻿
+namespace BCSC.Entities
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
