45479d6d41d2ea301e23e5b98abcbb9a4f8952fd
kerimpotuk@gmail.com <kerimpotuk@gmail.com>
2023-05-16 17:49:29 +0300

    Add DB Context

---
+++ BCSCData/DataBaseBContext.cs
@@ -0,0 +1,15 @@
+﻿using BCSC.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BCSC.Data
+{
+    public class DataBaseBContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
